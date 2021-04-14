2e728a24c08759b4c9e1e5140bde42d42a551ae6
Denis Lozhkin <ssdenis00@yandex.ru>
2021-04-14 18:50:14 +0300

    fix: форматировать файл js

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,6 @@
 let editBtn = document.querySelector('.profile__edit-btn');
 let popupVisible = document.querySelector('.popup');
 let closeBtn = document.querySelector('.popup__cross');
-
-
-
-
-
 let formElement = document.querySelector('.popup__form');
 let nameInput = formElement.querySelector('.popup__form-text_name');
 let jobInput = formElement.querySelector('.popup__form-text_job');
@@ -17,13 +12,12 @@ editBtn.onclick = function () {
   editBtn.onClick = popupVisible.classList.remove('popup_visible_off');
 }
 
-closeBtn.onclick =
-  function () {
-    closeBtn.onClick = popupVisible.classList.remove('popup_visible_on');
-    closeBtn.onClick = popupVisible.classList.add('popup_visible_off');
-    nameInput.value = nameProfile.textContent;
-    jobInput.value = jobProfile.textContent;
-  }
+closeBtn.onclick = function () {
+  closeBtn.onClick = popupVisible.classList.remove('popup_visible_on');
+  closeBtn.onClick = popupVisible.classList.add('popup_visible_off');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
+}
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
